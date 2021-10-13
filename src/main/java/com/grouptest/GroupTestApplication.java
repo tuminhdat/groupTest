44374ba9e2eb0205c40c8fbcaebd75440bcbd6cc
Dat Tu <dattuminh@gmail.com>
2021-10-12 19:12:17 -0700

    first init

---
+++ src/main/java/com/grouptest/GroupTestApplication.java
@@ -0,0 +1,13 @@
+package com.grouptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grouptest/GroupTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
